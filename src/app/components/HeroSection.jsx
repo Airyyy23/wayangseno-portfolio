6df8438c39c8856d641c76a0e566559c23a64543
Airyyy23 <wayangseno123@gmail.com>
2024-09-16 14:02:23 +0700

    p

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -1,22 +1,29 @@
-'use client'
-
+'use client';
 import React from 'react';
 import Image from 'next/image';
 import { TypeAnimation } from 'react-type-animation';
+import { motion } from 'framer-motion';
+import Link from 'next/link';
 
 const HeroSection = () => {
   return (
-    <section>
-      <div className="grid grid-cols-10 sm:grid-cols-12">
-        <div className="col-span-7 place-self-center text-center sm:text-left">
-          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
-            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+    <section className="py-12 lg:py-16 bg-[#121212]">
+      <div className="grid grid-cols-1 sm:grid-cols-12 gap-6">
+        {/* Left Text Section */}
+        <motion.div
+          initial={{ opacity: 0, scale: 0.9 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+          className="sm:col-span-7 text-center sm:text-left flex flex-col justify-center"
+        >
+          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-7xl font-extrabold">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-sky-600">
               Hello, I&apos;m{' '}
             </span>
             <br />
             <TypeAnimation
               sequence={[
-                'Wayangseno',
+                'Judy',
                 1000,
                 'Web Developer',
                 1000,
@@ -30,34 +37,45 @@ const HeroSection = () => {
               repeat={Infinity}
             />
           </h1>
-          <p className="text-[#adb7be] text-base sm:text-lg lg:text-xl mb-6">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam
-            voluptas unde nulla soluta? Veritatis earum obcaecati dolore. Ipsam
-            quod eum, est provident eaque tenetur sed, unde illum id,
-            accusantium voluptatibus!
+          <p className="text-[#ADB7BE] text-lg lg:text-xl mb-6">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
+            voluptuous.
           </p>
-          <div>
-            <button className="px-6 py-4 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white">
+          <div className="flex flex-col sm:flex-row gap-4 justify-center sm:justify-start">
+            <Link
+              href="/#contact"
+              className="px-6 py-4 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white"
+            >
               Hire Me
-            </button>
-            <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white mt-3">
-              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-3">
+            </Link>
+            <Link
+              href="/cv/cv.pdf"
+              className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white mt-3"
+            >
+              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-4 py-2">
                 Download CV
               </span>
-            </button>
+            </Link>
           </div>
-        </div>
-        <div className="col-span-5 place-self-center mt-4 lg:mt-0">
-          <div className="rounded-full  w-[300px] lg:w-[400px] h-[300px] lg:h-[400px] relative">
+        </motion.div>
+
+        {/* Right Image Section */}
+        <motion.div
+          initial={{ opacity: 0, scale: 0.9 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+          className="sm:col-span-5 flex justify-center"
+        >
+          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[350px] lg:h-[350px] relative overflow-hidden">
             <Image
               src="/images/photo.png"
               alt="hero image"
-              width={300}
-              height={300}
-              className="rounded-xl absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              className="object-cover"
+              fill
+              priority
             />
           </div>
-        </div>
+        </motion.div>
       </div>
     </section>
   );
